b0f74bed67126d3e00d3b9ec98207e9461210b85
Bruno  Alcaine <111032198+brunoalcaine@users.noreply.github.com>
2023-06-02 21:31:28 -0300

    primeiro commit, salvando pessoa

---
+++ app/src/main/java/com/example/dm2y2023projeto7/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.dm2y2023projeto7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnSalvar).setOnClickListener {
+            val nome = findViewById<EditText>(R.id.edtNome).text.toString()
+            val curso = findViewById<EditText>(R.id.edtCurso).text.toString()
+
+            val dados = hashMapOf(
+                "nome" to nome,
+                "curso" to curso
+            )
+
+            db.collection("pessoa").add(dados)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Erro!", Toast.LENGTH_SHORT).show()
+                }
+        }
+
+    }
+}
